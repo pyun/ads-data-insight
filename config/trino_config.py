d59b20932b39a01e72431ae4349c9b71ce7c8e9e
wanglw <wanglw@ushareit.com>
2025-08-14 15:55:20 +0800

    [feat] read TRINO configuration from system environment first. [feat] add ignore config rule about python and idea etc.

--- config/trino_config.py
+++ config/trino_config.py
@@ -1,6 +1,11 @@
+import os
 # Trino数据库连接配置
 TRINO_CONFIG = {
-    "TRINO_HOST": "172.31.38.156",
-    "TRINO_PORT": "8889", 
-    "TRINO_USER": "hadoop"
+    "TRINO_HOST": os.getenv("TRINO_HOST") or "172.31.38.156",
+    "TRINO_PORT": os.getenv("TRINO_PORT") or "8889",
+    "TRINO_USER": os.getenv("TRINO_USER") or "hadoop",
+    "TRINO_PASSWORD": os.getenv("TRINO_PASSWORD") or "",
+    "TRINO_CATALOG": os.getenv("TRINO_CATALOG") or "hive",
+    "TRINO_SCHEMA": os.getenv("TRINO_SCHEMA") or "default",
+    "TRINO_HTTP_SCHEME": os.getenv("TRINO_HTTP_SCHEME") or "http",
 }
